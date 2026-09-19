ebad44aec098c5b6d8e563bfee537aa9286d0e5a
Pascal Knoth <pascal@malach.it>
2026-09-19 05:53:46 +0200

    fix: remove startup configuration delimiters #2

--- src/http_server.rs
+++ src/http_server.rs
@@ -178,7 +178,7 @@ fn serve_listener_with_workers_tls_and_limits(
                     })
                     .http1_only();
                 configure_http(&mut server, limits);
-                println!("kagome listening on {address}");
+                println!("kagome listening on {address}\n");
                 server
                     .serve(application.into_make_service_with_connect_info::<SocketAddr>())
                     .await
