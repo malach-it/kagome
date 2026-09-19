5490d2c7428a226922d67ff987115d0044735322
Pascal Knoth <pascal@malach.it>
2026-09-19 05:51:25 +0200

    fix: remove startup configuration delimiters

--- src/main.rs
+++ src/main.rs
@@ -12,9 +12,7 @@ fn main() -> ExitCode {
 
 fn run() -> Result<(), Box<dyn Error>> {
     let config = kagome::config::Config::initialize()?;
-    println!("-----BEGIN KAGOME CONFIGURATION-----");
     println!("{}", config.redacted_summary());
-    println!("-----END KAGOME CONFIGURATION-----\n");
     kagome::templates::initialize()?;
 
     kagome::http_server::serve_with_workers(&config.server.address, config.server.workers)?;
